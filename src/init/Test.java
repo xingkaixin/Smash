eb8c1996bfc974bf8f28b626dd3a9afd691d8b7b
XingKaiXin <xingkaixin@gmail.co>
2014-08-06 17:40:25 +0800

    update

--- src/init/Test.java
+++ src/init/Test.java
@@ -1,21 +1,76 @@
 package init;
 
 
-
 /**
  * Created by Kevin on 2014/8/6.
  */
 public class Test {
     public static void main(String[] args) {
-        int i = 0;
-        while(i<10) {
+     /*   int i = 0;
+        int[] result = new int[2];
+        int strsmash = 0;
+        long start = System.currentTimeMillis();
+        while (result[1] <3) {
             smash a = new smash();
-            System.out.println(a.getmash());
+            strsmash = a.getmash();
             i++;
+            result = a.findnumber(1234);
+            //System.out.println("result: "+result[0] + "  " + result[1]);
         }
+        long end = System.currentTimeMillis();
+        System.out.println("SMASH: " + strsmash);
+        System.out.println("循此次数: " + i+"  耗时"+(end-start)/1000f+"秒");
+*/
+        for(int tt=0;tt<10;tt++) {
+            smash targetsmash = new smash();
+            int target = targetsmash.getmash();
+
+            System.out.println("要猜的数字是：" + target);
+            int i = 0;
+            int[] firstresult = new int[2];
+            int  firststrsmash = 0;
+            long start = System.currentTimeMillis();
+            while(firstresult[1]<3) {
+                smash a = new smash();
+                firststrsmash = a.getmash();
+                firstresult = targetsmash.findnumber(firststrsmash);
+
+                i++;
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("循环了"+ i +"次，耗时"+(end-start)/1000f+"秒。目前猜到的数字是："+firststrsmash);
+
+            String second = String.valueOf(firststrsmash);
+            int intsecond = Integer.parseInt(second);
+
+            int result = intsecond;
+            String secondtemp;
+            int intssecondtemp;
+
+            int[] secondresult = new int[2];
+
+            int c=0;
+            int n=1;
 
 
+            for(c=0;c<4;c++) {
 
+                for(n=1;n<10;n++) {
+                    secondtemp = second.substring(0,c) + n + second.substring(c+1);
+                    intssecondtemp = Integer.parseInt(secondtemp);
+                    secondresult = targetsmash.findnumber(intssecondtemp);
+                    if(secondresult[1]==4) {
+                        result = intssecondtemp;
+                    }
+
+                }
+            }
+
+            start = System.currentTimeMillis();
+            System.out.println("循环了"+ n*c +"次，耗时"+(start-end)/1000f+"秒。目前猜到的数字是："+result);
+            // System.out.println(result);
+
+        }
 
     }
 }
--- src/init/smash.java
+++ src/init/smash.java
@@ -26,12 +26,41 @@ public class smash {
 
     }
 
-    public String getmash() {
-        String mash = "";
-        for(int i=0, size = smash.size();i< size ;i++) {
-            mash += smash.get(i)+"";
+    public int getmash() {
+        int mash = 0 ;
+        for(int i=0,j=1000, size = smash.size();i< size ;i++) {
+            mash += smash.get(i)*j;
+            j = j/10;
         }
         return mash;
+    }
+
+    public int[] findnumber(int numbers) {
+        boolean flag_exist;
+        boolean flag_location;
+        int[] result = new int[2];
+        int exist = 0;
+        int correct = 0;
+        String strnumbers = numbers + "";
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        for (int i = 0, j = 1; i < 4; i++, j++) {
+            number.add(Integer.valueOf(strnumbers.substring(i, j)));
+        }
+        for (int i = 0; i < 4; i++) {
+            int k = number.get(i);
+            flag_exist = smash.contains(k);
+            if (flag_exist) {
+                exist++;
+                if (k == smash.get(i)) {
+                    correct++;
+                }
+            }
+
+
+        }
+        result[0] = exist;
+        result[1] = correct;
+        return result;
     }
 
 }
