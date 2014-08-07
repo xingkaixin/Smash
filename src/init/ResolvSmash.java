2c4eadfc5edb55fd6d0ec550aa11d5e585b4b3d8
XingKaiXin <xingkaixin@gmail.co>
2014-08-07 17:34:13 +0800

    add

---
+++ src/init/ResolvSmash.java
@@ -0,0 +1,20 @@
+package init;
+
+/**
+ * Created by Kevin on 2014/8/7.
+ */
+public class ResolvSmash {
+
+    static int GuessSmash = 1234;
+
+    static int firstround = 1234;
+    static int secondround = 5678;
+
+    int[] result;
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
